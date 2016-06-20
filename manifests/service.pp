194a7c00d429c5a8fcb35aa2877afba1b32ef1d9
amar krishna <amar266@gmail.com>
2016-06-21 00:17:52 +0530

    Enabled Cluster

--- manifests/service.pp
+++ manifests/service.pp
@@ -23,38 +23,37 @@ exec{ 'sleep 100 to stablize onos':
         command => 'sudo sleep 100;'
 }->
 
-## create onos cluster
-#if count($controllers_ip) > 1 {
-#  $ip1 = $controllers_ip[0]
-#  $ip2 = $controllers_ip[1]
-#  $ip3 = $controllers_ip[2]
-#  exec{ 'create onos cluster':
-#        command => "/opt/onos/bin/onos-form-cluster $ip1 $ip2 $ip3"
-#  }
-#}
+
 exec{ 'install openflow feature':
-        command => "/opt/onos/bin/onos 'feature:install onos-openflow'"
+        command => "/opt/onos/bin/onos 'feature:install onos-openflow'",
+        before => EXEC['create onos cluster']
 }->
 exec{ 'install openflow-base feature':
-        command => "/opt/onos/bin/onos 'feature:install onos-openflow-base'"
+        command => "/opt/onos/bin/onos 'feature:install onos-openflow-base'",
+        before => EXEC['create onos cluster']
 }->
 exec{ 'install onos-ovsdb-base feature':
-        command => "/opt/onos/bin/onos 'feature:install onos-ovsdb-base'"
+        command => "/opt/onos/bin/onos 'feature:install onos-ovsdb-base'",
+        before => EXEC['create onos cluster']
 }->
 exec{ 'install ovsdatabase feature':
-        command => "/opt/onos/bin/onos 'feature:install onos-ovsdatabase'"
+        command => "/opt/onos/bin/onos 'feature:install onos-ovsdatabase'",
+        before => EXEC['create onos cluster']
 }->
 exec{ 'install onos-ovsdb-provider-host feature':
-        command => "/opt/onos/bin/onos 'feature:install onos-ovsdb-provider-host'"
+        command => "/opt/onos/bin/onos 'feature:install onos-ovsdb-provider-host'",
+        before => EXEC['create onos cluster']
 }->
 exec{ 'install onos-drivers-ovsdb feature':
-        command => "/opt/onos/bin/onos 'feature:install onos-drivers-ovsdb'"
+        command => "/opt/onos/bin/onos 'feature:install onos-drivers-ovsdb'",
+        before => EXEC['create onos cluster']
 }->
 exec{ 'sleep 10 to stablize onos features':
         command => 'sudo sleep 10;'
 }->
 exec{ 'install vtn feature':
-        command => "/opt/onos/bin/onos 'feature:install onos-app-vtn-onosfw'"
+        command => "/opt/onos/bin/onos 'feature:install onos-app-vtn-onosfw'",
+        before => EXEC['create onos cluster']
 }->
 exec{ 'add onos auto start':
         command => 'sudo echo "onos">>/opt/service',
@@ -62,5 +61,37 @@ exec{ 'add onos auto start':
 }-> 
 exec{ 'set public port':
         command => "/opt/onos/bin/onos 'externalportname-set -n onos_port2'",
+        before => EXEC['create onos cluster']
+}->
+exec{ 'stabalize features':
+        command => "sudo sleep 300",
+        before => EXEC['create onos cluster']
 }
+
+if ($::onos_run == "true") {
+  if ($::hostname =='onos-ctrl1') {
+    if count($controllers_ip) > 1 {
+      $ip1 = $controllers_ip[0]
+      $ip2 = $controllers_ip[1]
+      $ip3 = $controllers_ip[2]
+      exec{ 'create onos cluster':
+           command => "/opt/onos/bin/onos-form-cluster $ip1  $ip2  $ip3",
+            creates => '/opt/onos/config/cluster.json'
+      }
+    }
+  }else{
+  exec{ 'create onos cluster':
+           command => "date",
+      }
+
+  }
+
+  }else{
+  
+   notify{$::onos_run:}
+  }
+
+
+
 }
+
