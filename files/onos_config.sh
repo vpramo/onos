198cc384c071e6bbcbc0fe536630b2df74e3f62d
Pramod Venkatesh <pramodvenkatesh@Pramods-MacBook-Pro.local>
2016-06-22 17:33:25 +0530

    made config changes

--- files/onos_config.sh
+++ files/onos_config.sh
@@ -1,9 +1,8 @@
 #!bin/bash
 set -eux
 
+sudo mkdir /opt/onos/config;
 sudo echo 'export ONOS_OPTS=debug' > /opt/onos/options;
 sudo echo 'export ONOS_USER=root' >> /opt/onos/options;
 sudo rm -rf /opt/onos/var;
-sudo rm -rf /opt/onos/config;
 sudo mkdir /opt/onos/var;
-sudo mkdir /opt/onos/config;
