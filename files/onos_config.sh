bf42dd3e2fd845dd24dfd623590ede8c348b987c
Pramod Venkatesh <pramodvenkatesh@Pramods-MacBook-Pro.local>
2016-06-24 14:05:13 +0530

    do not recreate directory

--- files/onos_config.sh
+++ files/onos_config.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 set -eux
-
-sudo mkdir /opt/onos/config;
+mkdir -p /opt/onos/config;
 sudo echo 'export ONOS_OPTS=debug' > /opt/onos/options;
 sudo echo 'export ONOS_USER=root' >> /opt/onos/options;
 sudo rm -rf /opt/onos/var;
