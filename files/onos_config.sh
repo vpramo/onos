4f4dccc32dbbf8daedb2edef62e8c21087a3b251
vpramo <vpramo@gmail.com>
2016-07-25 20:05:01 +0530

    Update onos_config.sh

--- files/onos_config.sh
+++ files/onos_config.sh
@@ -4,6 +4,5 @@ set -eux
 sudo echo 'export ONOS_OPTS=debug' > /opt/onos/options;
 sudo echo 'export ONOS_USER=root' >> /opt/onos/options;
 sudo rm -rf /opt/onos/var;
-sudo rm -rf /opt/onos/config;
 sudo mkdir /opt/onos/var;
-sudo mkdir /opt/onos/config;
+
